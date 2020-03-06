a41fd6d8727199d36cf83dfe0fc3a0e52c060a1c
frances <frncsbthn@gmail.com>
2020-03-05 22:44:06 -0800

    added height and width adjustment functionality

--- src/components/BoxGenerator.js
+++ src/components/BoxGenerator.js
@@ -4,23 +4,34 @@ import styles from "./BoxGen.module.css";
 const BoxGenerator = props => {
   const [state, setState] = useState({
     boxes: [],
-    color: ""
+    box: {
+      color: "",
+      height: 0,
+      width: 0
+    }
   });
 
   const onChangeHandler = event => {
     setState({
       ...state,
-      color: event.target.value
+      [event.target.name]: event.target.value
     });
   };
 
   const onSubmitHandler = event => {
     event.preventDefault();
     const newBoxes = state.boxes.slice();
-    newBoxes.push(state.color);
+    const newBox = {
+      color: state.color,
+      height: state.height,
+      width: state.width
+    };
+    newBoxes.push(newBox);
     setState({
-      ...state,
-      boxes: newBoxes
+      boxes: newBoxes,
+      color: "",
+      height: "",
+      width: ""
     });
   };
 
@@ -29,8 +40,33 @@ const BoxGenerator = props => {
       <div>
         <form onSubmit={onSubmitHandler}>
           <div>
-            <label>Color: </label>
-            <input className={styles.addBox} onChange={onChangeHandler} />
+            <div className={styles.formElements}>
+              <label>Color </label>
+              <input
+                name="color"
+                className={styles.addBox}
+                value={state.color}
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className={styles.formElements}>
+              <label>Height </label>
+              <input
+                name="height"
+                className={styles.addBox}
+                value={state.height}
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className={styles.formElements}>
+              <label>Width </label>
+              <input
+                name="width"
+                className={styles.addBox}
+                value={state.width}
+                onChange={onChangeHandler}
+              />
+            </div>
             <input
               className={styles.addButton}
               type="submit"
@@ -40,15 +76,16 @@ const BoxGenerator = props => {
           </div>
         </form>
       </div>
-      <div>
-        {state.boxes.map((color, i) => (
+
+      <div className={styles.boxes}>
+        {state.boxes.map(({ color, height, width }, i) => (
           <div
             key={i}
             className={styles.new_box}
             style={{
               backgroundColor: color,
-              width: 200,
-              height: 200
+              width: !width ? 200 : parseInt(width),
+              height: !height ? 200 : parseInt(height)
             }}
           ></div>
         ))}
